6e42a2b38d57e7996e0b60924a8e0b0bc8d1b2f4
Aräjtav <arajtav@arajtav.com>
2025-06-25 16:00:35 +0200

    feat: scan from entrypoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use std::{fs, path::PathBuf};
+
+#[derive(Parser, Debug)]
+struct Args {
+    /// Location from where to start the scan.
+    entrypoint: PathBuf,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+
+    let mut to_scan = vec![args.entrypoint];
+    let mut scanned = vec![];
+    while !to_scan.is_empty() {
+        let entry = to_scan.pop().unwrap();
+        let metadata = fs::metadata(&entry)?;
+
+        if !metadata.is_dir() {
+            scanned.push((entry, metadata.len()));
+            continue;
+        }
+        let dir = fs::read_dir(entry)?;
+        to_scan.append(
+            &mut dir
+                .map(|entry| entry.map(|entry| entry.path()))
+                .collect::<Result<Vec<_>, _>>()?,
+        );
+    }
+
+    println!("{scanned:?}");
+
+    Ok(())
 }
